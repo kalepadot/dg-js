004d1f992a0b995c956c28bfa28d2fb3d0f25156
Jeremy Kale Padot <jeremy@padot.us>
2023-01-28 21:59:12 -0700

    var and string

--- js/main.js
+++ js/main.js
@@ -2,3 +2,8 @@ console.log("console script");
 console.log(typeof "jeremy");
 console.log(typeof 42);
 console.log(typeof true);
+
+//strings
+const myVariable = "mathematics";
+//The length property
+console.log(myVariable.length);
