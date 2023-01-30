9a9f11239f778ba66a5f05fa6792da8486f954d0
Jeremy Kale Padot <jeremy@padot.us>
2023-01-29 18:38:30 -0700

    rps 1

--- js/main.js
+++ js/main.js
@@ -9,3 +9,23 @@ const myVariable = "mathematics";
 console.log(myVariable.length);
 console.log(myVariable.charAt(6));
 console.log(myVariable.indexOf("kale"));
+
+// Your First Interactive Game
+let playGame = confirm("Shall we play rock, paper, or scissors?");
+if (playGame) {
+  //play
+  let playerChoice = prompt("Please enter rock, paper, or scissors.");
+  if (playerChoice) {
+    let playerOne = playerChoice.trim().toLowerCase();
+    if (
+      playerOne === "rock" ||
+      playerOne === "paper" ||
+      playerOne === "scissors"
+    ) {
+      let computerChoice = Math.floor(Math.random() * 3 + 1);
+      let computer =
+        computerChoice === 1
+          ? "rock"
+          : computerChoice === 2
+          ? "paper"
+          : "scissors";
