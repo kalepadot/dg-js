0bd65c81b2c9071d5daa607c1da050374ba06216
Jeremy Kale Padot <jeremy@padot.us>
2023-01-30 19:16:03 -0700

    fix and result

--- js/main.js
+++ js/main.js
@@ -14,3 +14,29 @@ if (playGame) {
         const computerChoice = Math.floor(Math.random() * 3);
         const rpsArray = ["rock", "paper", "scissors"];
         const computer = rpsArray[computerChoice];
+        const result =
+          playerOne === computer
+            ? "Tie game!"
+            : playerOne === "rock" && computer === "paper"
+            ? `playerOne: ${playerOne}\nComputer: ${computer}\nComputer wins!`
+            : playerOne === "paper" && computer === "scissors"
+            ? `playerOne: ${playerOne}\nComputer: ${computer}\nComputer wins!`
+            : playerOne === "scissors" && computer === "rock"
+            ? `playerOne: ${playerOne}\nComputer: ${computer}\nComputer wins!`
+            : `playerOne: ${playerOne}\nComputer: ${computer}\nplayerOne wins!`;
+        alert(result);
+        playGame = confirm("Play Again?");
+        if (!playGame) alert("Ok, thanks for playing.");
+        continue;
+      } else {
+        alert("You didn't enter rock, paper, or scissors.");
+        continue;
+      }
+    } else {
+      alert("I guess you changed your mind. Maybe next time.");
+      break;
+    }
+  }
+} else {
+  alert("Ok, maybe next time.");
+}
