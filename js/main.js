6ea835dae4c3c0564e20d1b505b051f44d328b82
Jeremy Kale Padot <jeremy@padot.us>
2023-02-02 16:05:48 -0700

    arrow function propercase

--- js/main.js
+++ js/main.js
@@ -7,8 +7,9 @@ Math.random();
 
 //Function Declaration Syntax:
 //Accept an email stop at @
-const getUserNameFromEmail = (email) => {
-  return email.slice(0, email.indexOf("@"));
+
+const toProperCase = (name) => {
+  return name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
 };
 
-console.log(getUserNameFromEmail("playerOne@gmail.com"));
+console.log(toProperCase("jeremy"));
