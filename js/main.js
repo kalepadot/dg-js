b52dab79f7422c4943f67b88c48f7271ba0a2724
Jeremy Kale Padot <jeremy@padot.us>
2023-02-01 18:43:31 -0700

    having soup

--- js/main.js
+++ js/main.js
@@ -2,3 +2,8 @@
 
 //syntax
 //condition ? ifTrue : ifFalse ;
+
+let soup = "chicken noodle soup";
+let response = soup ? "yes, we have soup." : "sorry we have no soup today";
+
+console.log(response);
