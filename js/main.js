065a26007547ebf858cacf836ced1aeaa96a5304
Jeremy Kale Padot <jeremy@padot.us>
2023-02-01 18:42:47 -0700

    ternary syntax

--- js/main.js
+++ js/main.js
@@ -1,18 +1,4 @@
-let testScore = 89;
-let grade;
-let collegeStudent = true;
-if (testScore > 90) {
-  grade = "A";
-} else if (testScore >= 80) {
-  grade = "B";
-} else if (testScore >= 70) {
-  grade = "c";
-} else if (testScore >= 60) {
-  grade = "d";
-} else {
-  if (collegeStudent) {
-    grade = "u";
-  } else {
-    grade = "f";
-  }
-}
+// Conditionals: Ternary Operator
+
+//syntax
+//condition ? ifTrue : ifFalse ;
