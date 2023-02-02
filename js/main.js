ec81c2ea85389490baba09bb4876881729b23a40
Jeremy Kale Padot <jeremy@padot.us>
2023-02-01 18:24:11 -0700

    random letter of name challenge

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// Code Challenge
+
+//Write code that will return a random letter form your name
+
+console.log(typeof "jeremy");
+//this is a string
+
+console.log("jeremy".charAt(Math.floor(Math.random() * 4 + 1)));
