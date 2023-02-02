f1000983308161a72971287f7a5b7b57223ab107
Jeremy Kale Padot <jeremy@padot.us>
2023-02-02 15:49:28 -0700

    more loops and continue

--- js/main.js
+++ js/main.js
@@ -3,9 +3,15 @@ let name = "jeremy";
 
 let counter = 0;
 let myLetter;
-while (true) {
+while (counter <= 3) {
   myLetter = name[counter];
   console.log(myLetter);
+  if (counter === 1) {
+    counter += 2;
+    continue;
+  }
   if (myLetter === "m") break;
   counter++;
 }
+
+console.log(counter);
