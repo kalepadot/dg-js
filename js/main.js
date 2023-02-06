aaf084d1b36c7c99bd5b44cb1aac379feb752d59
Jeremy Kale Padot <jeremy@padot.us>
2023-02-05 19:30:33 -0700

    var let const scope lession

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
+//var, let, and const
 
+//global scope
+
+var x = 1;
+let y = 2;
+const z = 3;
+console.log(x);
+//local scope
+
+{
+  let y = 4;
+}
+
+//local scope
+function myFunc() {
+  const z = 5;
+}
