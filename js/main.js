d4eae970e3f1df446a14e019212367c063323a9d
Jeremy Kale Padot <jeremy@padot.us>
2023-02-07 18:02:53 -0700

    array types

--- js/main.js
+++ js/main.js
@@ -1,2 +1,18 @@
 // Arrays
 const myArray = [];
+
+//add elements to an array
+myArray[0] = "jeremy";
+myArray[1] = 1001;
+myArray[2] = false;
+
+//refer to an array
+console.log(myArray);
+
+//length property
+console.log(myArray.length);
+
+//last element in an array
+console.log(myArray[myArray.length - 1]);
+
+console.log(myArray[1]);
