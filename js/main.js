0c4f330bc98a7030559dbd23a42ed6ff4816c253
Jeremy Kale Padot <jeremy@padot.us>
2023-02-07 18:11:09 -0700

    splice replace

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ myArray[0] = "jeremy";
 myArray[1] = 1001;
 myArray[2] = false;
 
-myArray.splice(1, 1);
+myArray.splice(1, 1, 42);
 //instead of delete which makes that position empty
 console.log(myArray);
+console.log(myArray[1]);
