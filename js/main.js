85cbffef825d590f3f0a797dd642dc57f7bd0c5a
Jeremy Kale Padot <jeremy@padot.us>
2023-02-07 17:59:15 -0700

    clear lesson, arrays

--- js/main.js
+++ js/main.js
@@ -1,18 +1,2 @@
-//var, let, and const
-
-//global scope
-
-var x = 1;
-let y = 2;
-const z = 3;
-console.log(x);
-//local scope
-
-{
-  let y = 4;
-}
-
-//local scope
-function myFunc() {
-  const z = 5;
-}
+// Arrays
+const myArray = [];
