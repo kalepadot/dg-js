799e0827424e4e6d245cfecdd3a7d8063154abdf
Jeremy Kale Padot <jeremy@padot.us>
2023-02-07 18:03:55 -0700

    push to add to an array and remove last (pop)

--- js/main.js
+++ js/main.js
@@ -6,13 +6,10 @@ myArray[0] = "jeremy";
 myArray[1] = 1001;
 myArray[2] = false;
 
-//refer to an array
+myArray.push("school");
+
 console.log(myArray);
 
-//length property
-console.log(myArray.length);
+myArray.pop();
 
-//last element in an array
-console.log(myArray[myArray.length - 1]);
-
-console.log(myArray[1]);
+console.log(myArray);
